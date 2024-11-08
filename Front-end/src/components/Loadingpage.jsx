b60a56c96ae974cae99bffe8417d1b2baf166c2d
Winter0016 <chauquangphuc2604.2604@gmail.com>
2024-11-08 18:33:02 +0700

    add loading page

--- Front-end/src/components/Loadingpage.jsx
+++ Front-end/src/components/Loadingpage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 export const Loading = () => {
+
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-200">
+    <div className="flex justify-center items-center h-screen bg-customgray">
       <div className="animate-spin rounded-full h-16 w-16 border-4 border-t-transparent border-blue-500" />
-      <p className="text-lg font-medium text-gray-700">Loading...</p>
+      <p className="text-lg font-medium text-gray-300">Loading...</p>
     </div>
   );
 }
