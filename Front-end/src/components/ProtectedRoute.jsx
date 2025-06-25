5dbfacdd7ca09daa7a96f7fdd8c1d56def9834b9
Winter0016 <chauquangphuc2604.2604@gmail.com>
2025-06-25 11:44:50 +0700

    debugging

--- Front-end/src/components/ProtectedRoute.jsx
+++ Front-end/src/components/ProtectedRoute.jsx
@@ -3,6 +3,10 @@ import { Navigate } from 'react-router-dom';
 const ProtectedRoute = ({ children, loggedusername }) => {
   // Check if the loggedusername is null, undefined, or an empty string
   // console.log(`route protector`)
+  console.log(`loggedusername: ${loggedusername}`)
+  if(!loggedusername){
+    console.log(`protected route couldnt find loggedusername ${loggedusername}`)
+  }
   if (!loggedusername) {
     return <Navigate to="/404" replace />;
   }
